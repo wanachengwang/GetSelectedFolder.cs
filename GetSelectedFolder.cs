b8fd101c6c5b4ce6f5e05ac884e6514f766133f3
wana_chengwang <chengwang2000>
2017-11-03 11:52:55 +0800

    first commit

---
+++ GetSelectedFolder.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEditor;
+
+public class EditorExt{
+
+    private static EditorWindow GetWindowByName(string pName) {
+        UnityEngine.Object[] winList = Resources.FindObjectsOfTypeAll(typeof(EditorWindow));
+        foreach (UnityEngine.Object win in winList) {
+            if (win.GetType().ToString() == pName)
+                return ((EditorWindow)win);
+        }
+        return null;
+    }
+    public static string GetSelectedFolder() {
+        string selectedFolder = "";
+        EditorWindow projectWindow = GetWindowByName("UnityEditor.ProjectBrowser");
+        if(projectWindow != null) {
+            //Since this was an internal type of UnityEditor assembly, I need to pass the class and the assembly that the class belongs to Type.GetType.
+            Type t = Type.GetType("UnityEditor.ProjectBrowser,UnityEditor");
+            selectedFolder = (t.GetField("m_LastFolders", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(_projectWindow) as string[])[0];
+            //selectedFolder = t.GetField("m_SelectedPath", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(_projectWindow) as string;
+            //selectedFolder = AssetDatabase.GetAssetPath(Selection.activeObject);
+        }
+        return selectedFolder;
+    }
+}
